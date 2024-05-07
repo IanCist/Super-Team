6a2a820e50e436297abf504a2ee6883e7121b491
IanCist <iantcisternas@gmail.com>
2024-05-07 13:56:49 -0400

    updated js

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-const teamMembers = {
-    name:'Lionel Messi',
-    age: 36,
-    activePlayer: true,
-    position: 
-    strengths:
-    weaknesses:
-    skills:
-    biography:
-}
+const teamMembers = [
+    {
+        name: 'Lionel Messi',
+        age: 36,
+        activePlayer: true,
+        position: 'forward',
+        strengths: 'Ball Control',
+        weaknesses: 'heading ability',
+        skills: ['Dribbling', 'Shooting', 'Passing'],
+        biography: 'Lionel Messi is considered one of the greatest football players of all time...'
+    },
+]
 
-function generateTeamCards() {
+function generateTeamCards(){
+
     const teamCardsContainer = document.getElementById('teamCards')
 
     teamMembers.forEach(member => {
-        const card = document.createElement('div')
-        card.classList.add('col-md-4')
-
-        card.innerHTML = `
+         const card = document.createElement('div')
+         card.classList.add('col-md-4')
+         
+         card.innerHTML= `
             <div class="card">
-            <div class = "card-header">
-            ${member.name}
-            <div>
-            <div class="card-body">
-            <p><strong>Position:</strong> $(member.position) </p>
+                <div class = "card-header">
+                    ${member.name}
+                </div>
+                <div class="card-body">
+                    <p><strong>Position:</strong> ${member.position}</p>
+                </div>
             </div>
-        </div>
-        
-        `
-
+         
+         `
     teamCardsContainer.appendChild(card)
     });
-
-
 }
+
 
 window.onload = generateTeamCards()
